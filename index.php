4cd7645c7deedbed5b0f0cc7c806d146e3e5230c
itziar <mitziar.herlei.1@educa.jcyl.es>
2022-10-11 08:23:29 +0000

    actualizado

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Index</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+
+<header>
+  <?php
+  include ("header.html");
+  ?>
+  <hr>
+</header>
+
+<main>
+<aside>
+    <nav>
+      <dl>
+        <dt>
+          <a href="Tema1/index.php">Tema 1</a>
+        </dt>
+        <dd>
+          <a href="Tema1/Prácticas/">Prácticas</a>
+        </dd>
+        <dt>
+          <a href="Tema2/">Tema 2</a>
+        </dt>
+          <dd>
+            <a href="Tema2/CodigoClase/">Código clase</a>
+          </dd>
+          <dd>
+            <a href="Tema2/Practicas/">Prácticas</a>
+          </dd>
+      </dl>
+</nav>
+</aside>
+</main>
+
+<footer>
+<hr>
+  <?php
+    include ("footer.html");
+  ?>
+</footer>
+
+</body>
+</html>
