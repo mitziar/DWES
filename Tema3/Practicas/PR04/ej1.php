4c31e01e042ecf5ef669128d1e3171665b8abf37
itziar <mitziar.herlei.1@educa.jcyl.es>
2022-10-13 11:04:17 +0000

    traer

--- Tema3/Practicas/PR04/ej1.php
+++ Tema3/Practicas/PR04/ej1.php
@@ -7,8 +7,52 @@
  *  *****
  * 
  */
+// $cadena = null;
+//     $filas = 5;
+//     for ($i = 1; $i <= $filas; $i++) {
+//         for ($h = $i; $h <= $i; $h++) {
+//             $cadena .= "*";
+//         }
+//         for ($h = $i; $h <= $i; $h++) {
+//             $cadena .= "*";
+//         }
+//         echo $cadena."<br />";
+//     }
+ for ($fila=5;$fila<=1;$fila--){
+    for($blanco=1; $blanco<=$fila;$blanco++){
+        echo "&nbsp;";
+    }
+    for($asterisco=1;($fila*2)-1;$asterisco++){
+        echo "*";
+    }
+    echo "<br>";
+    }
 
- for ($fila=3;$fila<=1;$fila--){
+    /**
+ * Programa utilizando bucles que muestre la figura rombo
+ * 
+ *    *
+ *   ***
+ *  *****
+ *   ***
+ *    *
+ * 
+ */
+
     
- }
+ /**
+ * Programa utilizando bucles que muestre la figura rombo
+ * 
+ *    *
+ *   * *
+ *  *   *
+ *   * *
+ *    *
+ * 
+ */
+
+ //devover cambio a partir del valor del producto y luego valor con el que paga > valor producto
+
+ //pedir año, y decir si es bisiesto 
+ //bisiesto es multiplo de 4. Pero los multipolos de 100 no lo son, aunque los de 400 si
 ?>
