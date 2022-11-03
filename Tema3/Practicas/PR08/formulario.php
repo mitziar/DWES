5372910be3323ca9005458d6176583544639436e
mitziar <mitziar.herlei.1@educa.jcyl.es>
2022-11-03 22:21:09 +0000

    vc

--- Tema3/Practicas/PR08/formulario.php
+++ Tema3/Practicas/PR08/formulario.php
@@ -35,101 +35,6 @@
     <div class="row">
             <h4><a href="../../">Tema 3</a><a href="../">/Prácticas</a>/PR08/Formulario plantilla</h4>
     </div>
-<<<<<<< HEAD
-        <h3>Formulario plantilla</h3><hr>
-      <form action="formulario.php" method="post" enctype="multipart/form-data>
-        <p>
-            <!-- Pongo el for = que el id para que cuando selecione el label el input tome el foco-->
-            <label for="idAlfabetico">Alfabético</label>
-            <input type="text" name="alfabetico" id="idAlfabetico" value="<?php
-            if (!vacio('alfabetico') && enviado()){//si el nombre no está vacio y el formulario ha sido enviado         
-                    echo $_REQUEST['alfabetico'];//el value es
-                }
-            ?>">
-            <?php
-            if (vacio('alfabetico') && enviado())
-            {//si el nombre está vacio y el formulario ha sido enviado                 
-            ?>
-            <span>Debe introducir nombre</span>
-            <?php
-             }
-        ?>
-
-                </p>
-                <p>
-                    <label for="idAlfabeticoOpcional">Alfabético Opcional</label>
-                    <input type="text" name="alfabeticoOpcional" id="idAlfabeticoOpcional"value="<?php
-                    if (!vacio('alfabeticoOpcional') && enviado()){//si el nombre no está vacio y el formulario ha sido enviado         
-                            echo $_REQUEST['alfabeticoOpcional'];//el value es
-                        }
-                    ?>">
-                </p>
-                <p>
-                    <label for="idAlfanumerico">Alfanumérico</label>
-                    <input type="text" name="alfanumerico" id="idAlfanumerico" value="<?php
-                    if (!vacio('alfanumerico') && enviado()){//si el nombre no está vacio y el formulario ha sido enviado         
-                            echo $_REQUEST['alfanumerico'];//el value es
-                        }
-                    ?>">
-                    <?php
-                    if (vacio('alfanumerico') && enviado())
-                    {//si el nombre está vacio y el formulario ha sido enviado                 
-                    ?>
-                    <span>Debe introducir alfanumerico</span>
-                    <?php
-                    }
-                ?>
-                </p>
-                <p>
-                    <label for="idAlfanumericoOpcional">Alfanumérico Opcional</label>
-                    <input type="text" name="alfanumericoOpcional" id="idAlfanumericoOpcional" value="<?php
-                    if (!vacio('alfanumericoOpcional') && enviado()){//si el nombre no está vacio y el formulario ha sido enviado         
-                            echo $_REQUEST['alfanumericoOpcional'];//el value es
-                        }
-                    ?>">
-                </p>
-                <p>
-                    <label for="idFecha">Fecha</label>
-                    <input type="date" name="fecha" id="idFecha" value="<?php
-                    if (!vacio('fecha') && enviado()){//si el nombre no está vacio y el formulario ha sido enviado         
-                            echo $_REQUEST['fecha'];//el value es
-                        }
-                    ?>">
-                    <?php
-                    if (vacio('fecha') && enviado())
-                    {//si el nombre está vacio y el formulario ha sido enviado                 
-                    ?>
-                    <span>Debe introducir fecha</span>
-                    <?php
-                    }
-                ?>
-                </p>
-                <p>
-                    <label for="idFechaOpcional">Fecha Opcional</label>
-                    <input type="date" name="fechaOpcional" id="idFechaOpcional" value="<?php
-                    if(!vacio('fechaOpcional')&&enviado())
-                        echo $_REQUEST['fechaOpcional'];
-                    ?>">
-                </p>
-                <p>
-                    <label for="radioObligatorio">Radio Obligatorio: </label>
-                    <input type="radio" name="radioObligatorio" id="idRadio1" value="opcion1" <?php
-                        if(enviado() && existe("radioObligatorio") && $_REQUEST["radioObligatorio"]=="opcion1"){
-                            echo "checked";
-                        }
-                    ?>><label for="idRadio1">Opcion1</label>
-
-            <input type="radio" name="radioObligatorio" id="idRadio2" value="opcion2"<?php
-                if(enviado() && existe("radioObligatorio") && $_REQUEST["radioObligatorio"]=="opcion2"){
-                    echo "checked";
-                }
-            ?>><label for="idRadio2">Opcion2</label>
-            <input type="radio" name="radioObligatorio" id="idRadio3" value="opcion3"<?php
-                if(enviado() && existe("radioObligatorio") && $_REQUEST["radioObligatorio"]=="opcion3"){
-                    echo "checked";
-                }
-            ?>><label for="idRadio3">Opcion3</label>
-=======
     
         <?php
         if(validado()){
@@ -156,7 +61,6 @@
                         echo $_REQUEST['alfabetico'];//el value es
                     }
                 ?>">
->>>>>>> 148456c (formulario)
                 <?php
                     if (vacio('alfabetico') && enviado())
                     {//si el nombre está vacio y el formulario ha sido enviado                 
@@ -165,131 +69,6 @@
                     <?php
                     }
                 ?>
-<<<<<<< HEAD
-        </p>
-        <br>
-        <p>
-            <label for="checkboxElige">Elige al menos 1 y máximo 3: </label>
-            <input type="checkbox" name="checkboxElige[]" id="idCheckbox1" value="checkbox1"<?php
-            if(enviado()&&existe('checkboxElige')){
-                foreach ($_REQUEST['checkboxElige'] as $key) {
-                    if($key=='checkbox1')
-                        echo "checked";
-                }
-            }
-            ?>><label for="idCheckbox1">Check1</label>
-            <input type="checkbox" name="checkboxElige[]" id="idCheckbox2"value="checkbox2"<?php
-            if(enviado()&&existe('checkboxElige')){
-                foreach ($_REQUEST['checkboxElige'] as $key) {
-                    if($key=='checkbox2')
-                        echo "checked";
-                }
-            }
-            ?>><label for="idCheckbox2">Check2</label>
-            <input type="checkbox" name="checkboxElige[]" id="idCheckbox3"value="checkbox3"<?php
-            if(enviado()&&existe('checkboxElige')){
-                foreach ($_REQUEST['checkboxElige'] as $key) {
-                    if($key=='checkbox3')
-                        echo "checked";
-                }
-            }
-            ?>><label for="idCheckbox3">Check3</label>
-            <input type="checkbox" name="checkboxElige[]" id="idCheckbox4"value="checkbox4"<?php
-            if(enviado()&&existe('checkboxElige')){
-                foreach ($_REQUEST['checkboxElige'] as $key) {
-                    if($key=='checkbox4')
-                        echo "checked";
-                }
-            }
-            ?>><label for="idCheckbox4">Check4</label>
-            <input type="checkbox" name="checkboxElige[]" id="idCheckbox5"value="checkbox5"<?php
-            if(enviado()&&existe('checkboxElige')){
-                foreach ($_REQUEST['checkboxElige'] as $key) {
-                    if($key=='checkbox5')
-                        echo "checked";
-                }
-            }
-            ?>><label for="idCheckbox5">Check5</label>
-            <input type="checkbox" name="checkboxElige[]" id="idCheckbox6"value="checkbox6"<?php
-            if(enviado()&&existe('checkboxElige')){
-                foreach ($_REQUEST['checkboxElige'] as $key) {
-                    if($key=='checkbox6')
-                        echo "checked";
-                }
-            }
-            ?>><label for="idCheckbox6">Check6</label>
-            <?php
-            if(enviado()&&!existe('checkboxElige')){
-                echo '<span>Seleccione opcion</span>';
-            }
-            ?>
-        </p>
-        <p>
-            <label for="idNumeroTelefono">Nº Teléfono</label>
-            <input type="number" name="numeroTelefono" id="idNumeroTelefono" value=<?php
-            if(enviado() && !vacio('numeroTelefono')){
-                echo $_REQUEST['numeroTelefono'];}
-            ?>><?php
-            if(enviado() && vacio('numeroTelefono')){
-
-                echo "<span>Introduce telefono<span>";
-            }
-            ?>
-        </p>
-        <p>
-            <label for="idEmaill">Email</label>
-            <input type="text" name="email" id="idEmail" placeholder="alguien@email.com" value=<?php
-                if(enviado() && existe('email')){
-                    echo $_REQUEST['email'];
-                }
-            ?>>
-            <?php
-                if (enviado() && vacio('email')){
-                    echo "<span>Introduce email<span>";
-                }
-            ?>
-        </p>
-        <p>
-            <label for="idPass">Contraseña</label>
-            <input type="password" name="pass" id="idPass" value=<?php
-            if(enviado() && existe('pass')){
-                echo $_REQUEST['pass'];
-            }
-            ?>><?php
-            if(enviado() && vacio('pass')){
-                echo "<span>Introduce contraseña<span>";
-            }
-            ?>
-        </p>
-        <p>
-            <label for="idDocumento">Subir documento</label>
-            <input type="file" name="documento" id="idDocumento"><?php
-            if(enviado() && existe('documento')){
-                $rutaGuardado = "./uploads/";
-               
-                // Se le establece el nombre al archivo a guardar
-                $rutaConNombreFichero = $rutaGuardado .  $_FILES[$_REQUEST['documento']]['name'];
-
-                // Si se mueve el fichero del sitio temporal a la ruta especificada...
-               // if(move_uploaded_file($_FILES[$_REQUEST['documento']]['tmp_name'],$rutaConNombreFichero))
-                //{
-                 //   echo "<br>El fichero se ha guardado correctamente.<br>";
-                 //   print_r();
-
-                //}
-            }
-            ?>
-            <?php
-            if(enviado() && vacio('documento')){
-                echo "<span>Introduce documento<span>";
-            }
-            ?>
-        </p>
-        <p>
-            <input type="submit" value="Enviar" name="enviar">
-        </p>
-    </form>
-=======
             </p>
             <p>
                 <label for="idAlfabeticoOpcional">Alfabético Opcional</label>
@@ -516,7 +295,6 @@
     <?php
     }
     ?>
->>>>>>> 148456c (formulario)
     </div> <!-- /container -->
     <footer class="container" style="background-color: bisque;">
     <p><center>Página de Itziar</center></p>
