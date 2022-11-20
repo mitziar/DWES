3953406c5eb38cfe83fa89fddc09529da7da6691
mitziar <mitziar.herlei.1@educa.jcyl.es>
2022-11-20 11:52:46 +0000

    acabado

--- Tema3/Practicas/PR10/ejercicio2/alumno.php
+++ Tema3/Practicas/PR10/ejercicio2/alumno.php
@@ -37,93 +37,137 @@ include('validaciones.php');
         <div class="row">            
         <?php
         
-        if(isset($_REQUEST['guardar']) && validado()){
-          $posicion;
-          $nombreAlumno=$_REQUEST['nombreAlumno'];
-          $nuevasNotas=($nombreAlumno.';'.$_REQUEST['nota1'].';'.$_REQUEST['nota2'].';'.$_REQUEST['nota3']);
-            if( $fp=fopen('notas.csv','r+')){
-              while($notas=fgetcsv($fp,0,";")){
-                $posicion=ftell($fp);
-                foreach ($notas as $key => $value) {
-                  if($value==$nombreAlumno){
-                    fseek($fp,$posicion);
-                    fwrite($fp,$nuevasNotas);
-                  }
+        if(enviado() && validado()){//Si el formulario ha sido enviado y las notas introducidas son válidas
+          
+          $arrayNotas=array();//Guarda los registros del fichero. Cada elemento será un array con la información de cada alumno
+          $nombreAlumno = $_REQUEST['nombreAlumno'];//Nombre del alumno para el que se van a modificar las notas
+          if (!file_exists('notas.csv')) {
+            echo "<span>El fichero no existe</span>";
+          } else {
+            //Existe el fichero notas.csv. Lo abrimos para leer los registros.
+            if (!$fp = fopen('notas.csv', 'r')) {
+              //No se puede abrir el fichero notas.csv
+              echo "<span>No se puede abrir el fichero</span>";
+            } else {
+              //Mientras siga leyendo se guarda los datos del alumno en el arrayNotas
+              //notas es el registro de cada alumno
+              while ($notas = fgetcsv($fp, 0, ";")) {
+                array_push($arrayNotas,$notas);
+              } 
+              fclose($fp);//Cerramos el fichero cuando haya leido todos los registros
+              if (!$fp = fopen('notas.csv', 'w')) {//abrimos el fichero para escribir
+                echo "<span>No se puede abrir el fichero</span>";
+              }else {
+                //Escribimos todos los registros que hemos almacenado previamente en el fichero,´
+                // machacando la información que tenía.
+                foreach ($arrayNotas as $key => $value) { //recorremos los registros que tenemos guardados
+                if ($value[0] == $nombreAlumno) {
+                  //SI el nombre del alumno que estamos modificando coincide con el de el array de registros,
+                  //guardaremos los datos recogidos del formulario
+                  $value[1] = $_REQUEST['nota1'];
+                  $value[2] = $_REQUEST['nota2'];
+                  $value[3] = $_REQUEST['nota3'];
+                  fputcsv($fp, $value,';');
+                } else {
+                  //Si no coincide el nombre del alumno, guardamos los valores que tenia el fichero anteriormente
+                  fputcsv($fp, $value,';');
                 }
               }
-              fclose($fp);
-              header('Location: ./LeerNotas.php');
-              exit();
-               
-            }else{
-              echo "<span>No se puede abrir el fichero</span>";  
+              fclose($fp);//cerramos el fichero
+             }
+             //Enviamos al usuario la página donde ve todos los registro, donde podrá comprobar las modificaciones
+                header('Location: ./LeerNotas.php');
+                exit();
             }
+          }
         }else{
+          //Cuando el formulario no ha sido enviado ni validado, que puede ser:
+            //-porque es la primera vez que accede a ver los datos del alumno
+            //-porque ha fallado la validación de los campos. Existen campos erroneos.
             if(isset($_REQUEST['alumno'])){
+              //-Es la primera vez que entra, y el nombre del alumno se guarda en 'alumno'
               $nombreAlumno=$_REQUEST['alumno'];
             }else{
               $nombreAlumno=$_REQUEST['nombreAlumno'];
+              //-Ha enviado el formulario y ha fallado la validación. El alumno se guarda en nombreAlumno
             }
-            $cadenaMostrar;
+
+            //Mostramos los datos del alumno en el formulario
+            $cadenaMostrar;//guardará los datos del alumno en concreto
             if(!$fp=fopen('notas.csv','r')){
               echo "<h3>No se puede abrir el fichero</h3>";
             }else{
+              //Si abre el fichero que almacena los datos lo recorremos mientras lea
               while($notas=fgetcsv($fp,0,";")){
                 foreach ($notas as $element) {
+                  //para cada registro del fichero, si coincide el nombre del alumno, lo guardamos para mostrarlo
+                  //en los campos del formulario
                     if($notas[0]==$nombreAlumno){
                       $cadenaMostrar=$notas;
                     }
                   }
                 }
+                //Cuando haya leido todos los registros del fichero, cerramos el fichero.
                 fclose($fp);
               }
             }
+            //Patron que comprueba que cada nota empieza y acaba en un número del 0 al 9, y que tenga un mínimo de un
+            //dígito y un máximo de dos dígitos
             $patron='/^[0-9]{1,2}$/';
-            $errores=array();
-
-            ?><form action='alumno.php' method='get'>
+            $errores=array();//array que guardará los posibles errores que cometa el usuario al introducir las notas
+            ?><form action='alumno.php' method='post'>
                 <label>Alumno</label>
                 <input type="text" readonly name="nombreAlumno" value="<?php 
                 if(isset($_REQUEST['guardar']))
-                {
+                {//Mostramos el nombre del alumno en el input correspondiente
                   echo $_REQUEST['nombreAlumno'];
                 }else{
                   echo $cadenaMostrar[0];
                 }?>"><br>
                 <label>Notas</label>
                 <input type="text" name="nota1" value="<?php
+                //mostramos la primera nota, desde los valores del formulario si ha sido establecido
+                //o desde el registro del formulario leido, si es la primera vez que accede a esta página
                   if(!isset($_REQUEST['nota1'])){
                     echo $cadenaMostrar[1];
                   }else{
                     echo $_REQUEST['nota1'];
                   }
                     ?>"><?
+                    //Si ha sido enviado y la nota no es válida, guardamos el error para informar al usuario cuando intente guardar
                   if(enviado() && ($_REQUEST['nota1']<0 || $_REQUEST['nota1']>10) && !preg_match($patron,$_REQUEST['nota1'])){
                     array_push($errores,'Nota uno no válida <br>');
                   }
                 ?>
                 <input type="text" name="nota2" value="<?php
+                //mostramos la segunda nota, desde los valores del formulario si ha sido enviado
+                //o desde el registro del formulario leido, si es la primera vez que accede a esta página
                 if(!isset($_REQUEST['nota2'])){
                   echo $cadenaMostrar[2];
                 }else{
                   echo $_REQUEST['nota2'];
                 }?>"><?php
+                //Si ha sido enviado y la nota no es válida, guardamos el error para informar al usuario cuando intente guardar
                   if(enviado() && ($_REQUEST['nota2']<0 || $_REQUEST['nota2']>10) && !preg_match($patron,$_REQUEST['nota2'])){
                     array_push($errores,'Nota dos no válida <br>');
                   } 
                 ?>
                 <input type="text" name="nota3" value="<?php
+                //mostramos la tercera nota, desde los valores del formulario si ha sido enviado
+                //o desde el registro del formulario leido, si es la primera vez que accede a esta página
                 if(!isset($_REQUEST['nota3'])){
                   echo $cadenaMostrar[3];
                 }else{
                   echo $_REQUEST['nota3'];
                 }?>"><?php
+                //Si ha sido enviado y la nota no es válida, guardamos el error para informar al usuario cuando intente guardar
                   if(enviado() && ($_REQUEST['nota3']<0 || $_REQUEST['nota3']>10) && !preg_match($patron,$_REQUEST['nota3'])){
                     array_push($errores,'Nota tres no válida <br>');
                   } 
                 ?>
                 <input type="submit" value="guardar" name='guardar'>
             </form><?
+            //Si hemos almacenado algún error con los datos que ha enviado el usuario, se le muestran, al final del formulario.
             if(count($errores)!=0){
               foreach ($errores as $key => $value) {
                 echo '<span>'.$value.'</span>';
--- Tema3/Practicas/PR10/ejercicio2/validaciones.php
+++ Tema3/Practicas/PR10/ejercicio2/validaciones.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Comprueba que los valores que los valores que se envian en el formulario ($_REQUEST), excepto el valor
+ * del nombre del alumno y el del guardar, sean números de uno o dos dígitos y que esten entre 0 y 10.
+ * @return False si alguno de los número no son uno o dos digitos o no están entre 0 y 20.
+ * @return True si los numeros son digitos entre 0 y 10.
+ */
 function validado(){
     $errores= array();
     foreach ($_REQUEST as $key => $value) {
@@ -14,6 +20,12 @@ function validado(){
     }
     return false;
 }
+/**
+ * Comprueba que se haya enviado el formulario. Esto es, cuando está establecido el valor guardar en el 
+ * array que se genera al enviar el formulario ($_Request).
+ * @return False si el guardar no ha sido establecido.
+ * @return True si guardar ha sido establecido.
+ */
 function enviado(){
     if(!empty($_REQUEST['guardar'])){
         return true;
