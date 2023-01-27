d7ffc470ffbfa940bd2bbc278bbb7eeb2e7fa2e3
itziar <mitziar.herlei.1@educa.jcyl.es>
2023-01-27 13:23:21 +0000

    ghfh

--- PracticaMCV/controller/ProductoController.php
+++ PracticaMCV/controller/ProductoController.php
@@ -2,10 +2,11 @@
 if(isset($_REQUEST['comprar'])){
     if(empty($_REQUEST['unidades'])){
         $_SESSION['error']='Introduzca las unidades que desea comprar';
-        require_once $_SESSION['vista'];
+        //require_once $_SESSION['vista'];
     }elseif(!estaValidado()){
         $_SESSION['error']='Debe hacer login para comprar';
-        require_once $_SESSION['vista'];
+        //session controlador login y vista login
+        //require_once $_SESSION['vista'];
        }
     else{
         //venta
